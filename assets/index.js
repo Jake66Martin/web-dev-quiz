fc077b76826a5b29c39bcb82efa881837a0d3dea
leviathan <jake66martin@hotmail.com>
2023-09-02 15:22:29 -0400

    added first question elements with styling

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,61 @@
-var start = document.getElementById("#start-button");
+var start = document.querySelector("#startButton");
 
-var seconds = document.getElementById("#seconds-left");
+var startBtn = document.querySelector(".start-button");
+
+var seconds = document.querySelector("#secondsLeft");
+
+var quiz = document.querySelector(".quiz");
+
+var questionBtn = document.querySelector(".questionButtons");
 
 
 
-start.addEventListener("click", function() {
-prompt("hello world");
-})
+function startGame() {
+    
+quiz.innerHTML = "<h1>Choose the tag that corresponds with <br> the definition of an empty tag</h1>";
+startBtn.innerHTML = "";
+
+var button = document.createElement("button");
+button.textContent = "The p tag";
+questionBtn.appendChild(button);
+button.setAttribute("style", "background-color: black; color: white; font-size: 20px; width: 100px;")
+
+var button2 = document.createElement("button");
+button2.textContent = "The nav tag";
+questionBtn.appendChild(button2);
+button2.setAttribute("style", "background-color: black; color: white; font-size: 20px; width: 100px;")
+
+var button3 = document.createElement("button");
+button3.textContent = "The img tag";
+questionBtn.appendChild(button3);
+button3.setAttribute("style", "background-color: black; color: white; font-size: 20px; width: 100px;")
+
+var button4 = document.createElement("button");
+button4.textContent = "The h1 tag";
+questionBtn.appendChild(button4);
+button4.setAttribute("style", "background-color: black; color: white; font-size: 20px; width: 100px;")
+
+button = false;
+button2 = false;
+button3 = true;
+button4 = false;
+
+
+
+
+
+
+
+   
+   
+
+
+
+
+    
+   
+
+
+}
+
+start.addEventListener("click", startGame)
